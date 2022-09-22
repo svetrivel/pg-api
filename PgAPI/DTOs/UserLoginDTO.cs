c281eca9a435c1b03ef7d4057403c88f810e1599
vetrivel <vetrivel240597@gmail.com>
2022-09-22 19:18:12 +0530

    trest commit

--- PgAPI/DTOs/UserLoginDTO.cs
+++ PgAPI/DTOs/UserLoginDTO.cs
@@ -1,4 +1,4 @@
 public class UserLoginDTO {
-    public string UserName { get; set; }
-    public string Password { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
 }
